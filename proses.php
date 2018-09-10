973918597ffe6aa71bd8c8e8ea35be4983f174e4
maulinashintade <42725367+maulinashintade@users.noreply.github.com>
2018-09-10 09:57:28 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,33 @@
 <?php 
-//Tuliskan kode anda di bawah ini
+function baris_bintang ($value){
+
+	kolom($value);
+	echo "<br>";
+	$value--;
+	if ($value >= 1)
+		baris_bintang($value);
+}
+function kolom($batas, $awal=1){
+	echo "*";
+	$awal++;
+	if ($awal <= $batas) 
+		kolom($batas, $awal);
+}
+function ganjil_genap($value){
+	$genap = array();
+	$ganjil = array();
+	for ($i=1; $i < $nilai; $i++) { 
+		if ($i % 2==0) {
+			array_push($ganjil, $i);
+		}else{
+			array_push($genap, $i);
+		}
+		echo "ganjil : ";
+	}
+}
+	if (isset($_GET ['submit'])) {
+	$angka = isset($_GET['angka']) ? $_GET['angka'] : 0;
+	baris_bintang($value);
+}
 
 ?>
